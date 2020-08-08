d5bcc8bdab278e5e6ec321c8bebb91b67a6c6e1c
Nguyen Thanh Nam <thanhnam192@gmail.com>
2020-08-08 15:25:56 +0700

    lambda
    lambda

--- src/lambdas/test-lambda/index.js
+++ src/lambdas/test-lambda/index.js
@@ -1,6 +1,8 @@
 const Commons = require('../../../commons/CommonTest');
+const ENV_NAME = process.env.ENV_NAME;
 exports.handler = async (event) => {
-    console.log('HelloWorld');
+    console.log('Hello World');
+    console.log(ENV_NAME);
     Commons.test('nam nguyen');
     
 }
